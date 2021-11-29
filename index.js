a188ade158dd1c1059256d1cbbca361eca966027
RaccoonCode96 <waawoo13@gmail.com>
2021-11-30 00:48:25 +0900

    Mod: volumeCredits 함수화
    - volumeCreditsFor

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ function playFor(aPerformance) {
 	return plays[aPerformance.playID];
 }
 
+function volumeCreditsFor(aPerformance) {
+	let result = 0;
+	result += Math.max(aPerformance.audience - 30, 0);
+	if ('comedy' === playFor(aPerformance).type)
+		result += Math.floor(aPerformance.audience / 5);
+	return result;
+}
+
 function statement(invoice) {
 	let totalAmount = 0;
 	let volumeCredits = 0;
@@ -47,10 +55,8 @@ function statement(invoice) {
 
 	for (let perf of invoice.performances) {
 		// 포인트를 적립한다.
-		volumeCredits += Math.max(perf.audience - 30, 0);
 		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ('comedy' === playFor(perf).type)
-			volumeCredits += Math.floor(perf.audience / 5);
+		volumeCredits += volumeCreditsFor(perf);
 
 		// 청구 내역을 출력한다.
 		result += ` ${playFor(perf).name}: ${format(amoutFor(perf) / 100)} (${
@@ -58,6 +64,7 @@ function statement(invoice) {
 		}석)\n`;
 		totalAmount += amoutFor(perf);
 	}
+
 	result += `총액: ${format(totalAmount / 100)}\n`;
 	result += `적립 포인트: ${volumeCredits}점\n`;
 	return result;
