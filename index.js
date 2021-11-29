b0a593964f81753aaf5ae4cb2a56155d2337e3a2
RaccoonCode96 <waawoo13@gmail.com>
2021-11-29 23:46:52 +0900

    Mod : statement 쪼개기

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+/* statement 함수 쪼개기
+ * - 값 변경 없는 초기 변수를 추출한 함수의 매개변수로 전달하고,
+ * - 해당 값을 변경해서 받는 형식으로 변경
+ */
+
+function amoutFor(perf, play) {
+	// 합을 구한다.
+	let thisAmount = 0;
+	switch (play.type) {
+		case 'tragedy': // 비극
+			thisAmount = 40000;
+			if (perf.audience > 30) {
+				thisAmount += 1000 * (perf.audience - 30);
+			}
+			break;
+		case 'comedy': // 희극
+			thisAmount = 30000;
+			if (perf.audience > 20) {
+				thisAmount += 10000 + 500 * (perf.audience - 20);
+			}
+			thisAmount += 300 * perf.audience;
+			break;
+		default:
+			throw new Error(`알 수 없는 장르: ${play.type}`);
+	}
+	return thisAmount;
+}
+
 function statement(invoice, plays) {
 	let totalAmount = 0;
 	let volumeCredits = 0;
@@ -10,24 +38,7 @@ function statement(invoice, plays) {
 
 	for (let perf of invoice.performances) {
 		const play = plays[perf.playID];
-		let thisAmout = 0;
-
-		switch (play.type) {
-			case 'tragedy': // 비극
-				thisAmout = 40000;
-				if (perf.audience > 30) {
-					thisAmout += 1000 * (perf.audience - 30);
-				}
-				break;
-			case 'comedy': // 희극
-				thisAmout = 30000;
-				if (perf.audience > 20) {
-					thisAmout += 100000 + 500;
-				}
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${play.type}`);
-		}
+		let thisAmount = amoutFor(perf, play);
 
 		// 포인트를 적립한다.
 		volumeCredits += Math.max(perf.audience - 30, 0);
@@ -35,10 +46,10 @@ function statement(invoice, plays) {
 		if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
 		// 청구 내역을 출력한다.
-		result += ` ${play.name}: ${format(thisAmout / 100)} (${
+		result += ` ${play.name}: ${format(thisAmount / 100)} (${
 			perf.audience
 		}석)\n`;
-		totalAmount += thisAmout;
+		totalAmount += thisAmount;
 	}
 	result += `총액: ${format(totalAmount / 100)}\n`;
 	result += `적립 포인트: ${volumeCredits}점\n`;
