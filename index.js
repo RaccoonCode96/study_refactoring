8be76c67bfd4f179cda348a5a9bbf28c5142d7f3
RaccoonCode96 <waawoo13@gmail.com>
2021-12-02 22:12:11 +0900

    Mod: renderPlainText 함수로 만들기
    - text, html 형태로 나누어서 사용하고 싶음

--- index.js
+++ index.js
@@ -7,6 +7,10 @@
  */
 
 function statement(invoice) {
+	return renderPlainText(invoice);
+}
+/* -------------------------------------------------------------- */
+function renderPlainText(invoice) {
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
 	for (let perf of invoice.performances) {
