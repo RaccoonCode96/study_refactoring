9e2de64517ca2db0b6587c0cceabfc762941fd4a
RaccoonCode96 <waawoo13@gmail.com>
2021-12-02 22:03:05 +0900

    Mod: statement 함수 쪼개기 완료
    - 값 변경 없는 초기 변수를 추출한 함수의 매개변수로 전달하고,
     - 해당 값을 변경해서 받는 형식으로 변경
     - 임시 변수는 없애고 질의 함수 형태로 넣는다.
     - 함수의 반환 값에는 항상 result를 사용한다.
     - 함수 선언 바꾸기 : 핵심 느낌을 살릴 수 있는 식별자로 변경
     - 반복문 쪼개기 & 문장 슬라이드 하기 & 함수

--- index.js
+++ index.js
@@ -2,84 +2,97 @@
  * - 값 변경 없는 초기 변수를 추출한 함수의 매개변수로 전달하고,
  * - 해당 값을 변경해서 받는 형식으로 변경
  * - 임시 변수는 없애고 질의 함수 형태로 넣는다.
+ * - 함수의 반환 값에는 항상 result를 사용한다.
+ * - 함수 선언 바꾸기 : 핵심 느낌을 살릴 수 있는 식별자로 변경
  */
 
-function amoutFor(aPerformance) {
-	/*
-	 * - 함수의 반환 값에는 항상 result를 사용한다.
-	 */
-
-	// 합을 구한다.
-	let result = 0;
-	switch (playFor(aPerformance).type) {
-		case 'tragedy': // 비극
-			result = 40000;
-			if (aPerformance.audience > 30) {
-				result += 1000 * (aPerformance.audience - 30);
-			}
-			break;
-		case 'comedy': // 희극
-			result = 30000;
-			if (aPerformance.audience > 20) {
-				result += 10000 + 500 * (aPerformance.audience - 20);
-			}
-			result += 300 * aPerformance.audience;
-			break;
-		default:
-			throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-	}
-	return result;
-}
-
-function playFor(aPerformance) {
-	return plays[aPerformance.playID];
-}
-
-function volumeCreditsFor(aPerformance) {
-	let result = 0;
-	result += Math.max(aPerformance.audience - 30, 0);
-	if ('comedy' === playFor(aPerformance).type)
-		result += Math.floor(aPerformance.audience / 5);
-	return result;
-}
-
-// 함수 선언 바꾸기 : 핵심 느낌을 살릴 수 있는 식별자로 변경
-function usd(aNumber) {
-	return new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency: 'USD',
-		minimumFractionDigits: 2,
-	}).format(aNumber / 100);
-}
-
-function totalVolumeCredits(invoice) {
-	// 반복문 쪼개기 & 문장 슬라이드 하기
-	let volumeCredits = 0;
-	for (let perf of invoice.performances) {
-		volumeCredits += volumeCreditsFor(perf);
-	}
-	return volumeCredits;
-}
-
 function statement(invoice) {
-	let totalAmount = 0;
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
 	for (let perf of invoice.performances) {
-		// 포인트를 적립한다.
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-
-		// 청구 내역을 출력한다.
 		result += ` ${playFor(perf).name}: ${usd(amoutFor(perf))} (${
 			perf.audience
 		}석)\n`;
-		totalAmount += amoutFor(perf);
 	}
 
-	result += `총액: ${usd(totalAmount)}\n`;
+	result += `총액: ${usd(totalAmount(invoice))}\n`;
 	result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
 	return result;
+
+	/* -------------------------------------------------------------- */
+
+	function totalAmount(invoice) {
+		// 반복문 쪼개기 & 문장 슬라이드 하기 & 함수 추출
+		let result = 0;
+		for (let perf of invoice.performances) {
+			result += amoutFor(perf);
+		}
+		return result;
+	}
+
+	/* -------------------------------------------------------------- */
+
+	function totalVolumeCredits(invoice) {
+		// 반복문 쪼개기 & 문장 슬라이드 하기 & 함수 추출
+		let result = 0;
+		for (let perf of invoice.performances) {
+			result += volumeCreditsFor(perf);
+		}
+		return result;
+	}
+
+	/* -------------------------------------------------------------- */
+
+	function usd(aNumber) {
+		return new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+			minimumFractionDigits: 2,
+		}).format(aNumber / 100);
+	}
+
+	/* -------------------------------------------------------------- */
+
+	function volumeCreditsFor(aPerformance) {
+		let result = 0;
+		result += Math.max(aPerformance.audience - 30, 0);
+		if ('comedy' === playFor(aPerformance).type)
+			result += Math.floor(aPerformance.audience / 5);
+		return result;
+	}
+
+	/* -------------------------------------------------------------- */
+
+	function playFor(aPerformance) {
+		return plays[aPerformance.playID];
+	}
+
+	/* -------------------------------------------------------------- */
+
+	function amoutFor(aPerformance) {
+		let result = 0;
+		switch (playFor(aPerformance).type) {
+			case 'tragedy': // 비극
+				result = 40000;
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30);
+				}
+				break;
+			case 'comedy': // 희극
+				result = 30000;
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20);
+				}
+				result += 300 * aPerformance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+		}
+		return result;
+	}
 }
+
+/* --------------------------T E S T------------------------------------ */
 
 const invoices = require('./invoices.json');
 const plays = require('./plays.json');
