496a0eadc7a838775f8b3ea7835973d8a6cdebc2
RaccoonCode96 <waawoo13@gmail.com>
2021-12-02 23:37:37 +0900

    Mod: renderHtml 함수를 갖는 htmlStatement 생성

--- statement.js
+++ statement.js
@@ -1,5 +1,13 @@
 const { createStatementData } = require('./createStatementData');
 
+function usd(aNumber) {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2,
+	}).format(aNumber / 100);
+}
+
 exports.statement = function (invoice, plays) {
 	return renderPlainText(createStatementData(invoice, plays));
 
@@ -15,13 +23,24 @@ exports.statement = function (invoice, plays) {
 		result += `총액: ${usd(data.totalAmount)}\n`;
 		result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 		return result;
+	}
+};
 
-		function usd(aNumber) {
-			return new Intl.NumberFormat('en-US', {
-				style: 'currency',
-				currency: 'USD',
-				minimumFractionDigits: 2,
-			}).format(aNumber / 100);
+exports.htmlStatement = function (invoice, plays) {
+	return renderHtml(createStatementData(invoice, plays));
+
+	function renderHtml(data) {
+		let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+		result += '<table>\n';
+		result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+
+		for (let perf of data.performances) {
+			result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+			result += `<td>${usd(perf.amount)}</td></tr>\n`;
 		}
+		result += '</table>\n';
+		result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+		result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+		return result;
 	}
 };
