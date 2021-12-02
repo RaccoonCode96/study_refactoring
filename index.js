044e0af847fe66e37f68536642054f8a6663d68b
RaccoonCode96 <waawoo13@gmail.com>
2021-12-02 23:00:38 +0900

    Mod: totalVolumeCredits 함수 데이터 값으로 만들기

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function statement(invoice, plays) {
 	statementData.customer = invoice.customer;
 	statementData.performances = invoice.performances.map(enrichPerformance);
 	statementData.totalAmount = totalAmount(statementData);
+	statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
 	return renderPlainText(statementData);
 
@@ -23,36 +24,11 @@ function statement(invoice, plays) {
 		return result;
 	}
 
-	function volumeCreditsFor(aPerformance) {
+	function totalVolumeCredits(data) {
+		// 반복문 쪼개기 & 문장 슬라이드 하기 & 함수 추출
 		let result = 0;
-		result += Math.max(aPerformance.audience - 30, 0);
-		if ('comedy' === aPerformance.play.type)
-			result += Math.floor(aPerformance.audience / 5);
-		return result;
-	}
-
-	function playFor(aPerformance) {
-		return plays[aPerformance.playID];
-	}
-
-	function amountFor(aPerformance) {
-		let result = 0;
-		switch (aPerformance.play.type) {
-			case 'tragedy': // 비극
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-			case 'comedy': // 희극
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+		for (let perf of data.performances) {
+			result += perf.volumeCredits;
 		}
 		return result;
 	}
@@ -63,6 +39,40 @@ function statement(invoice, plays) {
 		result.amount = amountFor(result);
 		result.volumeCredits = volumeCreditsFor(result);
 		return result;
+
+		function volumeCreditsFor(aPerformance) {
+			let result = 0;
+			result += Math.max(aPerformance.audience - 30, 0);
+			if ('comedy' === aPerformance.play.type)
+				result += Math.floor(aPerformance.audience / 5);
+			return result;
+		}
+
+		function playFor(aPerformance) {
+			return plays[aPerformance.playID];
+		}
+
+		function amountFor(aPerformance) {
+			let result = 0;
+			switch (aPerformance.play.type) {
+				case 'tragedy': // 비극
+					result = 40000;
+					if (aPerformance.audience > 30) {
+						result += 1000 * (aPerformance.audience - 30);
+					}
+					break;
+				case 'comedy': // 희극
+					result = 30000;
+					if (aPerformance.audience > 20) {
+						result += 10000 + 500 * (aPerformance.audience - 20);
+					}
+					result += 300 * aPerformance.audience;
+					break;
+				default:
+					throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+			}
+			return result;
+		}
 	}
 }
 /* -------------------------------------------------------------- */
@@ -74,21 +84,12 @@ function renderPlainText(data) {
 	}
 
 	result += `총액: ${usd(data.totalAmount)}\n`;
-	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+	result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 	return result;
 
 	/* -------------------------------------------------------------- */
 
 	/* -------------------------------------------------------------- */
-
-	function totalVolumeCredits() {
-		// 반복문 쪼개기 & 문장 슬라이드 하기 & 함수 추출
-		let result = 0;
-		for (let perf of data.performances) {
-			result += perf.volumeCredits;
-		}
-		return result;
-	}
 
 	/* -------------------------------------------------------------- */
 
