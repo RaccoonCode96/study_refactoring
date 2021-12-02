9467dcf833f085cfc2b54e4241cd9b3d934cc82e
RaccoonCode96 <waawoo13@gmail.com>
2021-12-02 22:45:20 +0900

    Mod: amountFor 함수 값 중간 데이터 값으로 저장하기

--- index.js
+++ index.js
@@ -17,9 +17,32 @@ function statement(invoice, plays) {
 		return plays[aPerformance.playID];
 	}
 
+	function amountFor(aPerformance) {
+		let result = 0;
+		switch (aPerformance.play.type) {
+			case 'tragedy': // 비극
+				result = 40000;
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30);
+				}
+				break;
+			case 'comedy': // 희극
+				result = 30000;
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20);
+				}
+				result += 300 * aPerformance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+		}
+		return result;
+	}
+
 	function enrichPerformance(aPerformance) {
 		const result = Object.assign({}, aPerformance); // 얕은 복사
 		result.play = playFor(result);
+		result.amount = amountFor(result);
 		return result;
 	}
 }
@@ -28,9 +51,7 @@ function renderPlainText(data) {
 	let result = `청구 내역 (고객명: ${data.customer})\n`;
 
 	for (let perf of data.performances) {
-		result += ` ${perf.play.name}: ${usd(amoutFor(perf))} (${
-			perf.audience
-		}석)\n`;
+		result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
 	}
 
 	result += `총액: ${usd(totalAmount())}\n`;
@@ -43,7 +64,7 @@ function renderPlainText(data) {
 		// 반복문 쪼개기 & 문장 슬라이드 하기 & 함수 추출
 		let result = 0;
 		for (let perf of data.performances) {
-			result += amoutFor(perf);
+			result += perf.amount;
 		}
 		return result;
 	}
@@ -74,7 +95,7 @@ function renderPlainText(data) {
 	function volumeCreditsFor(aPerformance) {
 		let result = 0;
 		result += Math.max(aPerformance.audience - 30, 0);
-		if ('comedy' === playFor(aPerformance).type)
+		if ('comedy' === aPerformance.play.type)
 			result += Math.floor(aPerformance.audience / 5);
 		return result;
 	}
@@ -82,28 +103,6 @@ function renderPlainText(data) {
 	/* -------------------------------------------------------------- */
 
 	/* -------------------------------------------------------------- */
-
-	function amoutFor(aPerformance) {
-		let result = 0;
-		switch (aPerformance.play.type) {
-			case 'tragedy': // 비극
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-			case 'comedy': // 희극
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-		}
-		return result;
-	}
 }
 
 /* --------------------------T E S T------------------------------------ */
