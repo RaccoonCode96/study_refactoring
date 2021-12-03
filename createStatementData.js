fc06400fe9f7802dc7d734d59e14f2f027211946
RaccoonCode96 <waawoo13@gmail.com>
2021-12-03 23:52:45 +0900

    Mod: amountFor 함수를 class getter property화

--- createStatementData.js
+++ createStatementData.js
@@ -4,6 +4,28 @@ class PerformanceCalculator {
 		this.performance = aPerformance;
 		this.play = aPlay;
 	}
+
+	get amount() {
+		let result = 0;
+		switch (this.performance.play.type) {
+			case 'tragedy': // 비극
+				result = 40000;
+				if (this.performance.audience > 30) {
+					result += 1000 * (this.performance.audience - 30);
+				}
+				break;
+			case 'comedy': // 희극
+				result = 30000;
+				if (this.performance.audience > 20) {
+					result += 10000 + 500 * (this.performance.audience - 20);
+				}
+				result += 300 * this.performance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${this.performance.play.type}`);
+		}
+		return result;
+	}
 }
 
 exports.createStatementData = function (invoice, plays) {
@@ -54,25 +76,8 @@ exports.createStatementData = function (invoice, plays) {
 		}
 
 		function amountFor(aPerformance) {
-			let result = 0;
-			switch (aPerformance.play.type) {
-				case 'tragedy': // 비극
-					result = 40000;
-					if (aPerformance.audience > 30) {
-						result += 1000 * (aPerformance.audience - 30);
-					}
-					break;
-				case 'comedy': // 희극
-					result = 30000;
-					if (aPerformance.audience > 20) {
-						result += 10000 + 500 * (aPerformance.audience - 20);
-					}
-					result += 300 * aPerformance.audience;
-					break;
-				default:
-					throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-			}
-			return result;
+			return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+				.amount;
 		}
 	}
 };
