ff633302579f2d02f0fce0da38bf1213dee781d2
RaccoonCode96 <waawoo13@gmail.com>
2021-12-04 00:26:49 +0900

    Mod: PerformanceCalculator -> sub class로 분기
    - amount의 경우 조건부 로직이였는데, 이를 subClass로 지정하여 type별로 amount를 다르게 함

--- createStatementData.js
+++ createStatementData.js
@@ -1,8 +1,16 @@
 // 조건부 로직을 다형성으로 바꾸기
 
-// 생성자를 팩터리 함수로 바꾸기(생성자가 서브클래스의 인스턴스를 반환할 수 없기 때문)
+// 생성자를 팩터리 함수로 바꾸기(생성자가 서브클래스의 인스턴스를 반환할 수 없기 때문
+
 function createPerformanceCalculator(aPerformance, aPlay) {
-	return new PerformanceCalculator(aPerformance, aPlay);
+	switch (aPlay.type) {
+		case 'tragedy':
+			return new TragedyCalculator(aPerformance, aPlay);
+		case 'comedy':
+			return new ComedyCalculator(aPerformance, aPlay);
+		default:
+			throw new Error(`알 수 없는 경로: ${aPlay.type}`);
+	}
 }
 class PerformanceCalculator {
 	constructor(aPerformance, aPlay) {
@@ -11,25 +19,7 @@ class PerformanceCalculator {
 	}
 
 	get amount() {
-		let result = 0;
-		switch (this.play.type) {
-			case 'tragedy': // 비극
-				result = 40000;
-				if (this.performance.audience > 30) {
-					result += 1000 * (this.performance.audience - 30);
-				}
-				break;
-			case 'comedy': // 희극
-				result = 30000;
-				if (this.performance.audience > 20) {
-					result += 10000 + 500 * (this.performance.audience - 20);
-				}
-				result += 300 * this.performance.audience;
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${this.performance.play.type}`);
-		}
-		return result;
+		throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
 	}
 
 	get volumeCredits() {
@@ -37,6 +27,27 @@ class PerformanceCalculator {
 		result += Math.max(this.performance.audience - 30, 0);
 		if ('comedy' === this.play.type)
 			result += Math.floor(this.performance.audience / 5);
+		return result;
+	}
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+	get amount() {
+		let result = 40000;
+		if (this.performance.audience > 30) {
+			result += 1000 * (this.performance.audience - 30);
+		}
+		return result;
+	}
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+	get amount() {
+		let result = 30000;
+		if (this.performance.audience > 20) {
+			result += 10000 + 500 * (this.performance.audience - 20);
+		}
+		result += 300 * this.performance.audience;
 		return result;
 	}
 }
