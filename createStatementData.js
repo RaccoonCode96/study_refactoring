cc6d09878a5ed28500fa9e908febce1f3a3d474b
RaccoonCode96 <waawoo13@gmail.com>
2021-12-04 00:01:02 +0900

    Mod: volumeCreditsFor -> volumeCredits getter
    - local 함수를 class getter로 변경

--- createStatementData.js
+++ createStatementData.js
@@ -26,6 +26,14 @@ class PerformanceCalculator {
 		}
 		return result;
 	}
+
+	get volumeCredits() {
+		let result = 0;
+		result += Math.max(this.performance.audience - 30, 0);
+		if ('comedy' === this.play.type)
+			result += Math.floor(this.performance.audience / 5);
+		return result;
+	}
 }
 
 exports.createStatementData = function (invoice, plays) {
@@ -60,16 +68,8 @@ exports.createStatementData = function (invoice, plays) {
 		const result = Object.assign({}, aPerformance); // 얕은 복사
 		result.play = calculator.play;
 		result.amount = calculator.amount;
-		result.volumeCredits = volumeCreditsFor(result);
+		result.volumeCredits = calculator.volumeCredits;
 		return result;
-
-		function volumeCreditsFor(aPerformance) {
-			let result = 0;
-			result += Math.max(aPerformance.audience - 30, 0);
-			if ('comedy' === aPerformance.play.type)
-				result += Math.floor(aPerformance.audience / 5);
-			return result;
-		}
 
 		function playFor(aPerformance) {
 			return plays[aPerformance.playID];
